16a3037c6bafb43280896bec7d723cf6245bbf03
Meng Li <mengli98git@gmail.com>
2022-07-25 19:17:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Star from './Star.js';
+import {useState} from 'react';
 
 function App() {
+  const arr = [0, 1, 2, 3, 4];
+  const [cur, setCurNum] = useState(-1);
+  const handleMouseEnter = (num) => {
+    setCurNum(num);
+  }
+  const handleMouseLeave = () => {
+    setCurNum(-1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        arr.map(item => {
+          return <Star 
+                  key={item} 
+                  index = {item}
+                  curNum={cur} 
+                  handleMouseEnter={handleMouseEnter} 
+                  handleMouseLeave={handleMouseLeave}/>
+        })
+      }
     </div>
   );
 }
